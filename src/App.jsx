8004c3793ccb48d86b8a1bbb5dbbb15fd284b8d3
unknown <ariyeric@gmail.com>
2024-09-10 11:14:29 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Sidebar from './Components/Sidebar/Sidebar';
+import Header from './Components/Header/Header';
+
+
+const App = () => {
+
+  return (
+    <>
+    <Sidebar/>
+    <Header/>
+    </>
+  );
+};
+
+export default App;
