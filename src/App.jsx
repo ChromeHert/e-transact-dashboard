6643d0fdee781bd7e01dff43290f5a6925467786
unknown <ariyeric@gmail.com>
2024-09-14 16:20:10 +0100

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Sidebar from './Components/Sidebar/Sidebar';
 import Header from './Components/Header/Header';
 
 
 const App = () => {
+  const [darkMode, setDarkMode] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
   return (
     <>
-    <Sidebar/>
-    <Header/>
+    <div className={`${darkMode && "dark"}`}>
+    <Header toggleDarkMode={toggleDarkMode}
+      darkMode={darkMode}
+      toggleSidebar={toggleSidebar}/>
+
+      <Sidebar isSidebarOpen={isSidebarOpen}/>
+     </div> 
     </>
   );
 };
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
-const Sidebar = () => {
+const Sidebar = ({ isSidebarOpen }) => {
   return (
-    <div>Sidebar</div>
+    <>
+      <aside className={`fixed top-0 left-0 z-40 w-64 h-screen pt-20 bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700 transition-transform ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"}`}>
+        Sidebar
+      </aside>
+    </>
   )
 }
 
